b6d10463c2217122229dcdab570fe9458a0e2423
ondichokev <ondichokev@gmail.com>
2024-08-05 06:04:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive Methods
+function appendCat(cat) {
+  cats.push(cat);
+}
+
+function prependCat(cat) {
+  cats.unshift(cat);
+}
+
+function removeLastCat() {
+  cats.pop();
+}
+
+function removeFirstCat() {
+  cats.shift();
+}
+
+// Nondestructive Methods
+function getCatsWithNewCatAtEnd(cat) {
+  return [...cats, cat];
+}
+
+function getCatsWithNewCatAtBeginning(cat) {
+  return [cat, ...cats];
+}
+
+function getCatsWithoutLastCat() {
+  return cats.slice(0, -1);
+}
+
+function getCatsWithoutFirstCat() {
+  return cats.slice(1);
+}
